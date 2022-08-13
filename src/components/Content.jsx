9ff4f089a90e3e80c25411b30d99031941b00bb1
onurmaden <ronur.maden@gmail.com>
2022-08-13 21:58:55 +0300

    navigatehome

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,7 +3,7 @@ import Navbar from "components/Navbar";
 import Home from "views/Home";
 import Search from "views/Search";
 import Library from "views/Library";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 const Content = () => {
 	return (
@@ -11,9 +11,15 @@ const Content = () => {
 			<Navbar />
 			<div className="px-8 pt-2">
 			<Routes>
-				<Route exact path="/" element={<Home />} />
+				<Route path="/home" element={<Home />}/>
+				<Route path="/" element={<Navigate to="/home" />} />
 				<Route path="/search" element={<Search />} />
 				<Route path="/library" element={<Library />} />
+				<Route
+        		path="*"
+        		element={<Navigate to="/" replace />}
+   				 />
+				
 			</Routes>
 			</div>
 			
